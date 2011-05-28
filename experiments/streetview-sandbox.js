2b365b45b20d2f2795e3bd369863175ef5e0ee3f
mhausenblas <michael.hausenblas@deri.org>
2011-05-28 13:40:27 +0100

    SV experiment: integrated Aftab's dynamic marker

--- experiments/streetview-sandbox.js
+++ experiments/streetview-sandbox.js
@@ -23,39 +23,54 @@ var APPLICATION_STATUS = {
  "12":"APPEALED FINANCIAL"
 };
 
+// color-coded marker corresponding to the GPlan status above
+var MARKER_COLOR = {
+	 "0":"#006666",
+	 "1":"#006600",
+	 "2":"#00E000",
+	 "3":"#000066",
+	 "4":"#663300",
+	 "5":"#660000",
+	 "8":"#FF0000",
+	 "9":"#000000",
+	 "10":"#66FF00",
+	 "11":"#FF9900",
+	 "12":"#A30000"
+	};
+
 // this is dummy data, I made it up based on exitings PA, but the schema should be used:
 var data = [
-	{appref:'a',lat:53.270,lng:-9.104,appdate:2000,appstatus:8, appdesc:'construct an extension to house'},
-	{appref:'b',lat:53.2701,lng:-9.102,appdate:2001,appstatus:8, appdesc:'construct extension to house, again'},
-	{appref:'c',lat:53.2703,lng:-9.104,appdate:2002,appstatus:3, appdesc:'another construct'},
-	{appref:'d',lat:53.2702,lng:-9.103,appdate:2001,appstatus:3, appdesc:'testing'},
-	{appref:'e',lat:53.2702,lng:-9.1041,appdate:2002,appstatus:3, appdesc:'blah blah'},
+	{appref:'a',lat:53.270,lng:-9.104,appdate:2000,appstatus:0, appdesc:'construct an extension to house'},
+	{appref:'b',lat:53.2701,lng:-9.102,appdate:2001,appstatus:0, appdesc:'construct extension to house, again'},
+	{appref:'c',lat:53.2703,lng:-9.104,appdate:2003,appstatus:1, appdesc:'another construct'},
+	{appref:'d',lat:53.2702,lng:-9.103,appdate:2004,appstatus:2, appdesc:'testing'},
+	{appref:'e',lat:53.2702,lng:-9.1041,appdate:2005,appstatus:2, appdesc:'blah blah'},
 	{appref:'f',lat:53.2695,lng:-9.1039,appdate:2001,appstatus:3, appdesc:'dunno'},
-	{appref:'g',lat:53.2702,lng:-9.1039,appdate:2002,appstatus:3, appdesc:'got it!'},
-	{appref:'h',lat:53.2703,lng:-9.104,appdate:2002,appstatus:8, appdesc:'nah, next time'},
-	{appref:'i',lat:53.2703,lng:-9.103,appdate:2000,appstatus:8, appdesc:'fail fail fail'},
+	{appref:'g',lat:53.2702,lng:-9.1039,appdate:2006,appstatus:5, appdesc:'got it!'},
+	{appref:'h',lat:53.2703,lng:-9.104,appdate:2007,appstatus:8, appdesc:'nah, next time'},
+	{appref:'i',lat:53.2703,lng:-9.103,appdate:2010,appstatus:2, appdesc:'fail fail fail'},
 	{appref:'j',lat:53.270,lng:-9.102,appdate:2001,appstatus:3, appdesc:'yey, I did it'},
-	{appref:'k',lat:53.2702,lng:-9.102,appdate:2002,appstatus:8, appdesc:'oh oh'},
+	{appref:'k',lat:53.2702,lng:-9.102,appdate:2002,appstatus:9, appdesc:'oh oh'},
 	{appref:'l',lat:53.2705,lng:-9.1,appdate:2000,appstatus:8, appdesc:'more here'},
 	{appref:'m',lat:53.2701,lng:-9.102,appdate:2001,appstatus:3, appdesc:'sdfsdf'},
 	{appref:'n',lat:53.2703,lng:-9.103,appdate:2002,appstatus:8, appdesc:'ffffff'},
 	{appref:'o',lat:53.2702,lng:-9.101,appdate:2000,appstatus:8, appdesc:'fas fdsdfsd dfsdfsd sdfsdfsd sdfsdfs '},
 	{appref:'p',lat:53.27,lng:-9.1,appdate:2001,appstatus:3, appdesc:'yfff '},
 	{appref:'q',lat:53.2703,lng:-9.101,appdate:2002,appstatus:8, appdesc:'fsdfff  ffffff'},
-	{appref:'r',lat:53.2698,lng:-9.106,appdate:2001,appstatus:8, appdesc:'fffffff fdsfdffsdnlkndsfl dsfnsdlkfn dskfnsd'},
-	{appref:'s',lat:53.2697,lng:-9.102,appdate:2001,appstatus:3, appdesc:'yuu'},
-	{appref:'t',lat:53.2699,lng:-9.102,appdate:2002,appstatus:8, appdesc:'vsdv'},
-	{appref:'u',lat:53.2698,lng:-9.101,appdate:2000,appstatus:8, appdesc:'abcl'},
-	{appref:'v',lat:53.2697,lng:-9.103,appdate:2001,appstatus:3, appdesc:'123 abbbb fddfff///'},
-	{appref:'w',lat:53.2698,lng:-9.102,appdate:2002,appstatus:8, appdesc:'? >'},
-	{appref:'x',lat:53.2701,lng:-9.103,appdate:2000,appstatus:8, appdesc:'.....'},
-	{appref:'y',lat:53.2699,lng:-9.104,appdate:2001,appstatus:3, appdesc:'mmmmm'},
-	{appref:'z',lat:53.2703,lng:-9.102,appdate:2002,appstatus:8, appdesc:'.o(bah)'},
-	{appref:'1',lat:53.270,lng:-9.103,appdate:2000,appstatus:8, appdesc:'x.x.x.x.x.x.'},
-	{appref:'2',lat:53.2701,lng:-9.103,appdate:2001,appstatus:3, appdesc:'. . . . . .    ...     '},
-	{appref:'3',lat:53.2698,lng:-9.104,appdate:2002,appstatus:8, appdesc:'this is a very very very long description to test how far we can go if at all, don\'t look here'},
-	{appref:'4',lat:53.2704,lng:-9.101,appdate:2000,appstatus:8, appdesc:'fail fail sdfffffffffff'},
-	{appref:'5',lat:53.2704,lng:-9.103,appdate:2001,appstatus:3, appdesc:'LAST'}		
+	{appref:'r',lat:53.2698,lng:-9.106,appdate:2003,appstatus:9, appdesc:'fffffff fdsfdffsdnlkndsfl dsfnsdlkfn dskfnsd'},
+	{appref:'s',lat:53.2697,lng:-9.102,appdate:2004,appstatus:3, appdesc:'yuu'},
+	{appref:'t',lat:53.2699,lng:-9.102,appdate:2005,appstatus:4, appdesc:'vsdv'},
+	{appref:'u',lat:53.2698,lng:-9.101,appdate:2003,appstatus:8, appdesc:'abcl'},
+	{appref:'v',lat:53.2697,lng:-9.103,appdate:2000,appstatus:3, appdesc:'123 abbbb fddfff///'},
+	{appref:'w',lat:53.2698,lng:-9.102,appdate:2002,appstatus:10, appdesc:'? >'},
+	{appref:'x',lat:53.2701,lng:-9.103,appdate:2009,appstatus:11, appdesc:'.....'},
+	{appref:'y',lat:53.2699,lng:-9.104,appdate:2008,appstatus:11, appdesc:'mmmmm'},
+	{appref:'z',lat:53.2703,lng:-9.102,appdate:2002,appstatus:12, appdesc:'.o(bah)'},
+	{appref:'1',lat:53.270,lng:-9.103,appdate:2005,appstatus:12, appdesc:'x.x.x.x.x.x.'},
+	{appref:'2',lat:53.2701,lng:-9.103,appdate:2006,appstatus:1, appdesc:'. . . . . .    ...     '},
+	{appref:'3',lat:53.2698,lng:-9.104,appdate:2007,appstatus:2, appdesc:'this is a very very very long description to test how far we can go if at all, don\'t look here'},
+	{appref:'4',lat:53.2704,lng:-9.101,appdate:2009,appstatus:11, appdesc:'fail fail sdfffffffffff'},
+	{appref:'5',lat:53.2704,lng:-9.103,appdate:2010,appstatus:3, appdesc:'LAST'}		
 ];
 
 /////////////////////////////////////////////////////////////////////////////// 
@@ -186,7 +201,7 @@ function addMarker(record) {
 
 	(function(r) {
 
-		var yMarkerImage = new google.maps.MarkerImage("img/year-marker-"+ r.appstatus +"-"+ r.appyear +".png");
+		var yMarkerImage = new google.maps.MarkerImage(drawMarker(r.appyear,r.appstatus));
 
 		marker = new google.maps.Marker({
 			position: pos,
@@ -219,6 +234,41 @@ function addMarker(record) {
 		
 	})({'appyear':record.appdate, 'appstatus':record.appstatus, 'appref':record.appref, 'appdesc': record.appdesc});
 }
+
+
+function drawMarker(year,status){
+	// see http://www.html5canvastutorials.com and http://diveintohtml5.org/canvas.html for more tricks ...
+	year = year.toString().substring(2); // only take last two digits into account
+	var canvas = document.getElementById("dynamarker");
+	var context = canvas.getContext('2d');
+	context.font = "9pt Arial";
+	context.lineWidth = 1;
+	context.strokeStyle = "#fff";
+
+	// create the semi-circle
+	context.beginPath();
+	context.arc(25, 25, 18, 0, Math.PI, true);
+	context.closePath();
+	context.fillStyle = MARKER_COLOR[status];
+	context.fill();
+    context.stroke();
+
+	// create the pin
+	context.beginPath();
+	context.moveTo(25, 64);
+	context.lineTo(25, 26);
+	context.fillStyle = '#fff';
+	context.fill();
+    context.stroke();
+
+	// display year on the marker
+	context.fillStyle = "WHITE";
+	context.fillText(year, 18, 21);
+
+	return canvas.toDataURL("image/png");
+}
+
+
 
 function getDisplayPosition(lat, lng){
 	var pos = new google.maps.LatLng(lat, lng);
