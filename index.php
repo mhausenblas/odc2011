25b34812868a025d7044f2e2b39d925b77a27471
Richard Cyganiak <richard@cyganiak.de>
2011-06-04 00:49:31 +0100

    block access to misc files

--- index.php
+++ index.php
@@ -4,16 +4,20 @@ ini_set('display_errors', '1');
 include 'config.inc.php';
 include 'lib/db.inc.php';
 include 'lib/functions.inc.php';
+require_once 'lib/response.class.php';
 require_once 'lib/request.class.php';
 
 $request = new Request();
+$response = new Response($config['site_base'], $request->uri);
+
 if ($request->matches('/^$/')) {
   include 'templates/homepage.php';
   exit;
+} else if ($request->matches('/^(latest|all|near)/')) {
+  $GP = new GP();
+  $GP->sendAPIResponse();
+} else {
+  $response->error(404);
 }
-
-$GP = new GP();
-
-$GP->sendAPIResponse();
 
 ?>
